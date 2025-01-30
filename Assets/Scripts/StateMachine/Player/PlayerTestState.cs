c278353f306da7ac0acf8dbd1f717b93d9311bee
MetinBiter <145712932+Teyon-Tet@users.noreply.github.com>
2025-01-31 01:21:20 +0300

    Movement Blend Tree

--- Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -18,7 +18,17 @@ namespace Scripts.StateMachine.Player
             movement.x = stateMachine.InputReader.MovementValue.x;
             movement.y = 0;
             movement.z = stateMachine.InputReader.MovementValue.y;
-            stateMachine.transform.Translate(movement * deltaTime);
+
+            stateMachine.Controller.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+            if (stateMachine.InputReader.MovementValue == Vector2.zero)
+            {
+                stateMachine.Animator.SetFloat("FreeLookSpeed", 0, 0.1f, deltaTime);
+                return;
+            }
+            
+            stateMachine.Animator.SetFloat("FreeLookSpeed", 1, 0.1f, deltaTime);
+            stateMachine.transform.rotation = Quaternion.LookRotation(movement);
 
         }
 
