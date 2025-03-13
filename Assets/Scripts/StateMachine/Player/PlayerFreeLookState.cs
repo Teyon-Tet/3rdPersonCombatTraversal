bb981aa8ef473c8d259d7679e92a6508b5ff560d
MetinBiter <145712932+Teyon-Tet@users.noreply.github.com>
2025-03-13 23:00:19 +0300

    update

---
+++ Assets/Scripts/StateMachine/Player/PlayerFreeLookState.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Scripts.StateMachine.Player
+{
+    public class PlayerFreeLookState : PlayerBaseState
+    {
+        private readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed") ;
+        
+        public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+        public override void Enter()
+        {
+            
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            Vector3 movement = CalculateMovement();
+
+            stateMachine.Controller.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+            if (stateMachine.InputReader.MovementValue == Vector2.zero)
+            {
+                stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0, 0.1f, deltaTime);
+                return;
+            }
+            
+            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1, 0.1f, deltaTime);
+            stateMachine.transform.rotation = Quaternion.LookRotation(movement);
+
+        }
+
+        public override void Exit()
+        {
+
+        }
+
+        private Vector3 CalculateMovement()
+        {
+            Vector3 forward = stateMachine.MainCameraTransform.forward;
+            Vector3 right = stateMachine.MainCameraTransform.right;
+
+            forward.y = 0f;
+            right.y = 0f;
+            
+            forward.Normalize();
+            right.Normalize();
+            
+            return forward * stateMachine.InputReader.MovementValue.y + right * stateMachine.InputReader.MovementValue.x;
+        }
+
+    }
+}
--- Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -8,10 +8,13 @@ namespace Scripts.StateMachine.Player
         [field: SerializeField] public CharacterController Controller { get; private set; }
         [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+        
+        public Transform MainCameraTransform { get; private set; }
 
         private void Start()
         {
-            SwitchState(new PlayerTestState(this));
+            MainCameraTransform = Camera.main.transform;
+            SwitchState(new PlayerFreeLookState(this));
         }
     }
 }
