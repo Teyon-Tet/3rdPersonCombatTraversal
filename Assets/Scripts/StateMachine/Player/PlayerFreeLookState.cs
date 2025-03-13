814b1a6de3fc455a6120e49ca1fa686a506eb0b8
MetinBiter <145712932+Teyon-Tet@users.noreply.github.com>
2025-03-13 23:16:46 +0300

    Movement Refactoring

--- Assets/Scripts/StateMachine/Player/PlayerFreeLookState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerFreeLookState.cs
@@ -5,6 +5,7 @@ namespace Scripts.StateMachine.Player
     public class PlayerFreeLookState : PlayerBaseState
     {
         private readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed") ;
+        private const float AnimatorDampTime = 0.1f;
         
         public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
@@ -21,13 +22,18 @@ namespace Scripts.StateMachine.Player
 
             if (stateMachine.InputReader.MovementValue == Vector2.zero)
             {
-                stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0, 0.1f, deltaTime);
+                stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0, AnimatorDampTime, deltaTime);
                 return;
             }
             
-            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1, 0.1f, deltaTime);
-            stateMachine.transform.rotation = Quaternion.LookRotation(movement);
+            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1, AnimatorDampTime, deltaTime);
+            FaceMovementDirection(movement, deltaTime);
 
+        }
+
+        private void FaceMovementDirection(Vector3 movement, float deltaTime)
+        {
+            stateMachine.transform.rotation = Quaternion.Lerp(stateMachine.transform.rotation, Quaternion.LookRotation(movement), deltaTime * stateMachine.RotationDamping); 
         }
 
         public override void Exit()
--- Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -8,6 +8,7 @@ namespace Scripts.StateMachine.Player
         [field: SerializeField] public CharacterController Controller { get; private set; }
         [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+        [field: SerializeField] public float RotationDamping{ get; private set; }
         
         public Transform MainCameraTransform { get; private set; }
 
